fb0a27f6dc24f1893074caba1d713b979e4c19cd
Steve Rector <steverector27@gmail.com>
2017-08-14 14:19:11 -0700

    Creates larger test suite for users and items

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,10 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   let :item {Item.new(title: "Kingdom Hearts", category: "game", genre: "fantasy", length: 30)}
+  let :example_user {User.create(email: "steverector27@gmail.com", username: "stussy446", password: "123")}
   let :invalid_item {Item.new(genre: "fantasy", length: 30)}
 
   it 'Is invalid without a title and category' do 
     expect(item.valid?).to eq(true)
     expect(invalid_item.valid?).to eq(false)
+  end
+
+  it 'has many user items' do 
+    item.save
+    expect(item.user_items.length).to eq(0)
+    item.user_items.create(user: example_user)
+    expect(item.user_items.length).to eq(1)
+  end
+
+  it 'has many users' do 
+    item.save 
+    expect(item.users.length).to eq(0)
+    item.users << example_user
+    expect(item.users.length).to eq(1)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,10 +4,25 @@ RSpec.describe User, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   let :user {User.new(email: "steverector27@gmail.com", username: "stussy446", password: "123")}
   let :invalid_user {User.new(email: "steverector27@gmail.com", username: "stussy446", password: "123")}
+  let :example_item {Item.create(title: "Kingdom Hearts", category: "game", genre: "fantasy", length: 30)}
 
   it "Is valid with a unique email and invalid without a unique email" do 
     expect(user.valid?).to eq(true)
     user.save 
     expect(invalid_user.valid?).to eq(false)
+  end
+
+  it 'has many user_items' do 
+    user.save
+    expect(user.user_items.length).to eq(0)
+    user.user_items.create(item: example_item)
+    expect(user.user_items.length).to eq(1)
+  end
+
+  it 'has many items' do 
+    user.save 
+    expect(user.items.length).to eq(0)
+    user.items << example_item
+    expect(user.items.length).to eq(1)
   end
 end
