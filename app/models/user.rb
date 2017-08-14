dd2d7c6def9f6d6397c7fff9cd6233faf296d284
Steve Rector <steverector27@gmail.com>
2017-08-14 12:49:05 -0700

    Create basic tests for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, :username, :password, :rank, presence: true
+  validates :email, :username, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  let :user {User.new(email: "steverector27@gmail.com", username: "stussy446", password: "123")}
+  let :invalid_user {User.new(email: "steverector27@gmail.com", username: "stussy446", password: "123")}
+
+  it "Is valid with a unique email and invalid without a unique email" do 
+    expect(user.valid?).to eq(true)
+    user.save 
+    expect(invalid_user.valid?).to eq(false)
+  end
 end
