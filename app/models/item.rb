43e403bc4ce1c7be51ad1ec39039d7b024347539
Steve Rector <steverector27@gmail.com>
2017-08-15 13:51:51 -0700

    Adds items routes, refactors print_item method as a method of item class

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,13 @@ class Item < ApplicationRecord
 
   has_many :logs
   has_many :users, through: :logs
+
+  def print_item
+    if category != "book"
+       "#{title} -- Genre: #{genre} -- Length: #{length} hours"
+    else
+       "#{title} -- Genre: #{genre} -- Length: #{length} pages"
+    end
+  end
+
 end
