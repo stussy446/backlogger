481a0af6262ac2bc595d3926d656c00145a3c7b5
Steve Rector <steverector27@gmail.com>
2017-08-16 11:55:55 -0700

    Alters seed data for more items

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@ genres = ["horror", "fantasy", "adventure", "science fiction"]
 
 User.create(email: "steverector27@gmail.com", username: "stussy446", password: "123")
 
-10.times do 
+50.times do 
   User.create(email: Faker::Internet.email, username: Faker::Internet.user_name, password: "123")
-  Item.create(title: Faker::ElderScrolls.region, category: categories.sample, genre: genres.sample, length: 30)
+  Item.create(title: Faker::Team.name, category: categories.sample, genre: genres.sample, length: 30)
 end
 
 
